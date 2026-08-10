b0da93e45f5909f1016fa76691b63b8d5e9bad73
HeYao <1507856020@qq.com>
2026-08-10 14:56:55 +0800

    Update build_gsm8k_bench_dataset.sh

--- build_gsm8k_bench_dataset.sh
+++ build_gsm8k_bench_dataset.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-SOURCE_FILE="/home/t00937989/datasets/gsm8k/test.jsonl"
+SOURCE_FILE="/root/.cache/modelscope/hub/datasets/grade_school_math/test.jsonl"
 TOKENIZER_PATH="/home/weights/DeepSeek-V4-Pro-w4a8-mtp"
 DATASET_ROOT="/home/t00937989/datasets/gsm8k"
 PAGE_SIZE="128"
