b8d087d5548fa0acfa526785b03bafa7fc803e4f
HeYao <1507856020@qq.com>
2026-09-14 15:02:07 +0800

    Update test_npu_enable_streaming_session.py

--- datasets/test_npu_enable_streaming_session.py
+++ datasets/test_npu_enable_streaming_session.py
@@ -24,7 +24,7 @@ from sglang.srt.utils import kill_process_tree
 from sglang.srt.utils.hf_transformers_utils import get_tokenizer
 from sglang.test.ascend.test_ascend_utils import LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH
 from sglang.test.ci.ci_register import register_npu_ci
-#LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH = "/home/weights/Llama-3.2-1B-Instruct"
+# LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH = "/home/weights/Llama-3.2-1B-Instruct"
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
