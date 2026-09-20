cff15395d1f5adb811650eae68730ae3e185e417
HeYao <1507856020@qq.com>
2026-09-20 15:10:03 +0800

    Update 112_single_test.sh

--- B070/qwen3_5/112_single_test.sh
+++ B070/qwen3_5/112_single_test.sh
@@ -2,12 +2,7 @@
 # 轻量级 bench_serving 单独测试
 set -euo pipefail
 export PYTHONPATH=/home/zhm/0729_sglang/alter/sglang/python:$PYTHONPATH
-# export PYTHONPATH=/home/zhm/0729_sglang/baseline/sglang/python:$PYTHONPATH
-# export PYTHONPATH=/home/zhm/0729_sglang/matsueyu/sglang/python:$PYTHONPATH
-
-# MODEL_PATH="/home/zhm/myweights/Qwen3.5-35B-A3B"
 MODEL_PATH=/home/weights/Qwen3.5-122B
-# MODEL_PATH="/home/weights/Qwen3.5-35B-A3B"
 HOST=127.0.0.1
 PORT=8964
 
@@ -21,11 +16,10 @@ python3 -m sglang.benchmark.serving \
   --random-input-len 2048 \
   --random-output-len 32768 \
   --random-range-ratio 1 \
-  --max-concurrency 208 \
-  --num-prompts 208 \
+  --max-concurrency 224 \
+  --num-prompts 224 \
   --request-rate inf \
   --seed 1234 \
   --tokenize-prompt
 
 echo "single test done."
-  # --random-output-len 32768 \
